7b6f780f75ffa645a228e58676a1ceab3f07e90c
Patryk <psidoruk@mobitouch.net>
2020-11-19 08:22:16 +0100

    initial commit

---
+++ src/main/java/net/mobitouch/testweatherservice/TestWeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package net.mobitouch.testweatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWeatherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWeatherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/mobitouch/testweatherservice/TestWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mobitouch.testweatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
